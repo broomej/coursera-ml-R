0e7a1c1ecb09622174ffd1fb85b77b05c776129a
Jai Broome <broome.jai@gmail.com>
2016-06-20 22:30:45 -0400

    1st section complete regexp partially implemented

--- ex6/ex6_chunks.R
+++ ex6/ex6_chunks.R
@@ -21,3 +21,37 @@ gkGenerator <- function (sigma = 1) {
     }
     return(new("rbfkernel", .Data = gk, kpar = list(sigma = sigma)))
 }
+
+## @knitr test-params
+testParams <- function(train, val, Cs, sigs){
+    paramPerformance <- data.frame()
+    for(C in Cs){
+        for(sig in sigs){
+            svm <- ksvm(V3 ~ .,
+                           data = train,
+                           C = C,
+                           type = "C-svc",
+                           kernel = gkGenerator,
+                           kpar = list(sigma = sigTransform(sig)))
+            pred <- predict(svm, val[, 1:ncol(val) - 1])
+            valError <- sum(pred != val[, ncol(val)]) / length(pred)
+            paramPerformance <- rbind(paramPerformance,
+                                      c(C, sig, error(svm), valError))
+        }
+    }
+    colnames(paramPerformance) <- c("C", "sigma", "train.error", "val.error")
+    return(paramPerformance)
+}
+
+## @knitr pre-process
+preProcess <- function(email){
+    # More help from kaleko on github
+    # https://github.com/kaleko/CourseraML/blob/master/ex6/ex6_spam.ipynb
+    email <- tolower(email)
+    email <- gsub('<[^<>]+>', ' ', email)
+    email <- gsub('[0-9]+', 'number', email)
+    email <- gsub('(http|https)://[^\\s]*', 'httpaddr', email) # leaves httpaddrspace.com/
+    email <- gsub('[^\\s]+@[^\\s]+', 'emailaddr', email) # leaves groupname-unsubsemailaddrs.com
+    email <- gsub('[$]+', 'dollar ', email)
+    return(email)
+}
